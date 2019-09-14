44fda2e2f67a486ce3acbcac248a6e2fd95868e9
Yuzhong Huang <yuzhongh@usc.edu>
2019-09-14 00:10:03 -0700

    add docs

--- main_mlp.py
+++ main_mlp.py
@@ -69,15 +69,17 @@ index_female_true_placeholder = tf.placeholder(tf.int32, [None])
 index_female_false_placeholder = tf.placeholder(tf.int32, [None])
 
 # w is the importance of female
-w_raw = tf.Variable(0.0)
-w = tf.math.sigmoid(w_raw)
+# use clip instead of sigmoid to avoid saturation. Make training on w faster
+# but it have a problem: once w go out side [0, 1], it will lost graident and couldn't go back
+raw_w = tf.Variable(0.5, name='w')
+w = tf.clip_by_value(raw_w, 0, 1)
 
 # alpha: importance of imparity loss
 # beta: importance of imparity loss + outcome loss
 alpha = 0.5
 beta = 0.5
 
-L1_output = tf.layers.dense(X_placeholder, DIM_HIDDEN, activation=tf.nn.leaky_relu)
+L1_output = tf.layers.dense(X_placeholder, DIM_HIDDEN, activation=tf.nn.tanh)
 output = tf.layers.dense(L1_output, DIM_OUTPUT, activation=None)
 
 prob = tf.nn.softmax(output)
@@ -98,27 +100,29 @@ pred = tf.math.argmax(prob, axis=1)
 diff = tf.to_float(pred) - Y_placeholder[:, 1]
 accuracy = 1 - tf.math.reduce_mean(tf.math.abs(diff))
 
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=Y_placeholder, logits=output))
-loss_all = (1-beta)*loss + beta * (alpha * loss_imparity + (1-alpha) * loss_outcome)
+loss_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=Y_placeholder, logits=output))
+loss_total = beta * (alpha * loss_imparity + (1-alpha) * loss_outcome) + (1-beta)*loss_entropy
 
-for v in tf.trainable_variables():
-	loss_all += 1e-4 * tf.nn.l2_loss(v) + 1e-6 * tf.losses.absolute_difference(v, tf.zeros(tf.shape(v)))
+# remove regulization on w
+variables = [v for v in tf.trainable_variables() if v != raw_w]
+for v in variables:
+	loss_total += 1e-4 * tf.nn.l2_loss(v) + 1e-6 * tf.losses.absolute_difference(v, tf.zeros(tf.shape(v)))
 
 lr = tf.Variable(0.01, name='lr', trainable=False)
 lr_decay_op = lr.assign(lr * 0.95)
 optimizer = tf.train.AdamOptimizer(lr)
-train_op = optimizer.minimize(loss_all)
+train_op = optimizer.minimize(loss_total)
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
 
 	wait = 0
-	smallest_loss_train = float('inf')
-	n_lr_decay = 5
+	smallest_loss_total_train = float('inf')
+	patience_lr_decay = 5
 
 	for epoch in range(1000):
-		loss_train, w_train, loss_imparity_train, loss_outcome_train, accuracy_train, _train_step = sess.run(
-			[loss, w, loss_imparity, loss_outcome, accuracy, train_op],
+		w_train, loss_total_train, loss_entropy_train, accuracy_train, loss_imparity_train, loss_outcome_train, train_step = sess.run(
+			[w, loss_total, loss_entropy, accuracy, loss_imparity, loss_outcome, train_op],
 				feed_dict={
 					X_placeholder: X_train,
 					Y_placeholder: Y_train,
@@ -131,8 +135,8 @@ with tf.Session() as sess:
 				}
 		)
 
-		loss_test, loss_imparity_test, loss_outcome_test, accuracy_test = sess.run(
-			[loss, loss_imparity, loss_outcome, accuracy],
+		loss_total_test, loss_entropy_test, accuracy_test, loss_imparity_test, loss_outcome_test = sess.run(
+			[loss_total, loss_entropy, accuracy, loss_imparity, loss_outcome],
 				feed_dict={
 					X_placeholder: X_test,
 					Y_placeholder: Y_test,
@@ -145,17 +149,18 @@ with tf.Session() as sess:
 				}
 		)
 
-		if loss_train <= smallest_loss_train:
-			smallest_loss_train = loss_train
+		if loss_total_train <= smallest_loss_total_train:
+			smallest_loss_total_train = loss_total_train
 			wait = 0
 			print('New smallest')
 		else:
 			wait += 1
 			print('Wait {}'.format(wait))
-			if wait % n_lr_decay == 0:
+			if wait % patience_lr_decay == 0:
 				sess.run(lr_decay_op)
 				print('Apply lr decay, new lr: %f' % lr.eval())
 
-		print(epoch, w_train, loss_train, accuracy_train, loss_imparity_train, loss_outcome_train, loss_test, accuracy_test, loss_imparity_test, loss_outcome_test)
+		print(f'Epoch: {epoch}, W: {w_train}\ntotal_train: {loss_total_train}, entropy_train: {loss_entropy_train}, accuracy_train: {accuracy_train}, imparity_train: {loss_imparity_train}, outcome_train: {loss_outcome_train}\ntotal_test : {loss_total_test}, entropy_test : {loss_entropy_test}, accuracy_test : {accuracy_test}, imparity_test : {loss_imparity_test}, outcome_test : {loss_outcome_test}\n')
+
 	pdb.set_trace()
-	print('OK')
+	print('Pause before exit')
